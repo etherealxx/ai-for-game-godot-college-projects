bb0bfe648a12777bccd62e6a6f2b5d687576172c
Etherealxx <64251396+etherealxx@users.noreply.github.com>
2024-11-17 22:15:28 +0700

    2nd assignment fixed, 11th assignment wip
    - big agent still try to pass the wall

--- 2nd Assignment _ Run From Player/scripts/runner_agents_horde.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@export var target_chaser_character : NodePath
-
-func _ready() -> void:
-	for runner in get_children():
-		#if "chaser_character" in runner:
-		runner.chaser_character = target_chaser_character
